34994601d3258b2a49234476d3de5359e13877ca
Selim-Ramdani <selimcodeur@gmail.com>
2023-06-28 10:29:43 +0200

    backlight OK loop pictures & param

--- app/main.js
+++ app/main.js
@@ -1,52 +1,43 @@
-webOS.service.request("luna://com.lg.app.signage.scapservice/", {
-  method: "setPictureMode",
-  parameters: {
-    param: "game",
-  },
-  onSuccess: function (ret) {
-    console.log(ret.message);
-  },
-  onFailure: function (err) {
-    console.log(JSON.stringify(err));
-  },
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("container");
+  var img = document.querySelector(".photo");
+  console.log(img);
 
-webOS.service.request("luna://com.lg.app.signage.scapservice/", {
-  method: "getPictureMode",
-  onSuccess: function (ret) {
-    console.log("getPictureMode: " + ret.returnData);
-  },
-  onFailure: function (err) {
-    console.log(JSON.stringify(err));
-  },
-});
+  var pictures = [
+    { photo: "./images/01.png", param: 100 },
+    { photo: "./images/02.png", param: 75 },
+    { photo: "./images/03.png", param: 65 },
+    { photo: "./images/04.png", param: 60 },
+    { photo: "./images/05.png", param: 55 },
+    { photo: "./images/06.png", param: 45 },
+    { photo: "./images/07.png", param: 40 },
+    { photo: "./images/08.png", param: 35 },
+    { photo: "./images/09.png", param: 25 },
+  ];
 
-webOS.service.request("luna://com.lg.app.signage.scapservice/", {
-  method: "getCurrentTime",
-  onSuccess: function (ret) {
-    console.log("getCurrentTime: " + ret.returnData);
-  },
-  onFailure: function (err) {
-    console.log(JSON.stringify(err));
-  },
-});
+  var i = 0;
+  var l = pictures.length - 1;
 
-webOS.service.request("luna://com.lg.app.signage.scapservice/", {
-  method: "getServerProperty",
-  onSuccess: function (ret) {
-    console.log("getServerProperty: " + ret.returnData);
-  },
-  onFailure: function (err) {
-    console.log(JSON.stringify(err));
-  },
-});
+  (function loop() {
+    if (i > l) {
+      i = 0;
+    }
 
-webOS.service.request("luna://com.lg.app.signage.scapservice/", {
-  method: "setPictureProperty",
-  onSuccess: function (ret) {
-    console.log("getPictureProperty: " + ret.returnData);
-  },
-  onFailure: function (err) {
-    console.log(JSON.stringify(err));
-  },
+    webOS.service.request("luna://com.lg.app.signage.scapservice/", {
+      method: "setPictureProperty",
+      parameters: { param: pictures[i].param },
+      onSuccess: function (ret) {
+        console.log("setPictureProperty: " + ret.returnData);
+      },
+      onFailure: function (err) {
+        console.log(JSON.stringify(err));
+      },
+    });
+
+    img.src = pictures[i].photo;
+    console.log(pictures[i].photo);
+    console.log(pictures[i].param);
+    loopTimer = setTimeout(loop, 4000);
+    ++i;
+  })();
 });
